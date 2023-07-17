23d715824ae9a1c10e95d123d253c12d08030399
cesar velasco <caacesar.ariza@gmail.com>
2023-07-17 18:22:53 -0500

    practica

---
+++ practica de sql server/practicaSqlServer.sql
@@ -0,0 +1,10 @@
+/*
+*info de lo que se va realizar en la base de datos 
+* 1. crear el modelo de un login de aplicacion 
+* 2. crear vista, procedimientos, tigger, job
+* 3.TSQL revisar que es.
+*/
+create schema develop;
+
+
+/*1 punto*/
